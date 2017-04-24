684209cfed12373c0838824fda3313426a22e566
abshriva <shaky2108@gmail.com>
2017-04-24 23:44:17 +0530

    Add
    Add

---
+++ Assignment/Assignment_4.3-1.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function carDetail(car) {
+    console.log(" The car details are : Model " + car.model + " , \n     Engine " + car.engine + " and the horespower is " + car.horsepower);
+}
+exports.carDetail = carDetail;
+;
+function CarInfo() {
+    console.log('========``````CAR SPCIFICTIONS`````=======');
+}
+exports.CarInfo = CarInfo;
+;
---
+++ Assignment/Assignment_4.3-2.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Assignment_4_3_1_1 = require("../TYPESCRIPT-3/Assignment_4.3-1");
+var Assignment_4_3_1_2 = require("../TYPESCRIPT-3/Assignment_4.3-1");
+var myCar;
+myCar = { model: "Wagon R", engine: "K10B", horsepower: 1000 };
+Assignment_4_3_1_2.CarInfo();
+Assignment_4_3_1_1.carDetail(myCar);
